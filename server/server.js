a6bb367da77b58a4edcb74d17e9f49ebadb83c03
Ibiyemi Abiodun <ibiyemi@intulon.com>
2017-02-18 13:44:56 -0800

    fixed the webserver

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use('/', express.static('../client'));
+app.use('/', express.static(path.join(__dirname, '../client')));
 app.listen(process.env.PORT || 3001);
