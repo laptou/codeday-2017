aa5150b6e5fc96b1830caaf1acfd7d24aaa6757a
Ibiyemi Abiodun <ibiyemi@intulon.com>
2017-02-19 00:35:54 -0800

    added local multiplayer

--- client/js/game.js
+++ client/js/game.js
@@ -31,7 +31,11 @@ var vector = {
         return vector.mult(vec, 1 / (vector.len(vec) || 1));
     }
 };
-let tints = [0xFF9999, 0xFFFF99, 0x99FF99, 0x9999FF, 0xFFCC99, 0xFF99CC];
+let tints = [0xFF9999, 0x99FF99, 0xFFFF99, 0x9999FF, 0xFFCC99, 0xFF99CC];
+let keybinds = [
+    [37, 39, 38, 40, 13],
+    [65, 68, 87, 83, 70]
+];
 
 class Keyboard {
     constructor(keyCode) {
@@ -149,7 +153,9 @@ class Game {
 
                 break;
             case 2:
-                this.player.update(time, dtime);
+                for (let player of this.players) {
+                    player.update(time, dtime);
+                }
 
                 for (let bomb of this.bombs) {
                     bomb.update(time, dtime);
@@ -262,17 +268,17 @@ class Game {
         // #region grid generation
 
         this.bounds = { x: 96, y: 140, width: (game.resolution.x - 192), height: (game.resolution.y - 256) };
-        this.bounds.size = Math.min(Math.floor(this.bounds.height / 8), Math.floor(this.bounds.width / 12));
+        this.bounds.size = Math.min(Math.floor(this.bounds.height / 9), Math.floor(this.bounds.width / 13));
         this.bounds.x = game.resolution.x / 2 - 6 * this.bounds.size;
         this.bounds.y = game.resolution.y / 2 - 4 * this.bounds.size;
-        this.bounds.width = 12 * this.bounds.size;
-        this.bounds.height = 8 * this.bounds.size;
+        this.bounds.width = 13 * this.bounds.size;
+        this.bounds.height = 9 * this.bounds.size;
 
         this.grid = new PIXI.Container();
         this.grid.position.set(this.bounds.x, this.bounds.y);
 
-        let hrange = 12,
-            vrange = 8;
+        let hrange = 13,
+            vrange = 9;
 
         var darkTile = new PIXI.Graphics();
         darkTile.beginFill(0, 0.25);
@@ -311,7 +317,10 @@ class Game {
             this.tiles.push(bombTile);
         }
 
-        this.player = new Player(this, 1, this.bounds.size);
+        this.players = [new Player(this, 0, this.bounds.size), new Player(this, 1, this.bounds.size)];
+
+        this.players[1].x = 12;
+        this.players[1].y = 8;
 
         for (var x = 1; x < hrange; x += 2) {
             for (var y = 1; y < vrange; y += 2) {
@@ -344,12 +353,14 @@ class Player {
      * @param {Number} index
      * @param {Number} size
      */
-    constructor(game, index, size) {
+    constructor(game, index, size, x, y) {
         this.game = game;
         this.sprite = new PIXI.Sprite(PIXI.utils.TextureCache["player-front"]);
         this.sprite.tint = tints[index];
         this.sprite.width = size;
         this.sprite.height = size;
+        this.sprite.x = 0;
+        this.sprite.y = 0;
         this.vx = 0;
         this.vy = 0;
         this.x = 0;
@@ -359,12 +370,13 @@ class Player {
 
         this.game.grid.addChild(this.sprite);
         
+        var keybind = keybinds[index];
         this.keyboard = {
-            left: new Keyboard(37),
-            right: new Keyboard(39),
-            up: new Keyboard(38),
-            down: new Keyboard(40),
-            enter: new Keyboard(13),
+            left: new Keyboard(keybind[0]),
+            right: new Keyboard(keybind[1]),
+            up: new Keyboard(keybind[2]),
+            down: new Keyboard(keybind[3]),
+            enter: new Keyboard(keybind[4]),
             last: null
         };
 
@@ -483,6 +495,9 @@ class Player {
 
                 this.move.direction = null;
             }
+        } else {
+            this.sprite.x = this.x * this.game.bounds.size;
+            this.sprite.y = this.y * this.game.bounds.size;
         }
     }
 
